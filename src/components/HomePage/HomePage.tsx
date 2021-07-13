5f5ffe3166fae8271a0d2434e53095eeaee0ca6c
Arciiix <artur.nowak100@gmail.com>
2021-07-13 17:02:33 +0200

    Install necessary basic dependencies

---
+++ src/components/HomePage/HomePage.tsx
@@ -0,0 +1,9 @@
+function HomePage() {
+  return (
+    <div>
+      <span>Monity</span>
+      <span>TODO: Make this page</span>
+    </div>
+  );
+}
+export default HomePage;
--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+import * as serviceWorker from "./serviceWorker";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { MuiThemeProvider } from "@material-ui/core";
+import { createTheme } from "@material-ui/core/styles";
+
+const theme = createTheme({
+  palette: {
+    type: "dark",
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <MuiThemeProvider theme={theme}>
+      <Provider store={store}>
+        <CssBaseline />
+        <App />
+      </Provider>
+    </MuiThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
+//TODO:
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
