163669681dbb5bfafe05e1ce3a53b1e4f0b6eac3
Arciiix <artur.nowak100@gmail.com>
2021-07-26 17:59:34 +0200

    Create the app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 //Components
 import HomePage from "./components/HomePage/HomePage";
 import Login from "./components/Account/Login/Login";
 import Register from "./components/Account/Register/Register";
+import Overview from "./components/App/Overview/Overview";
 
 function App() {
   return (
@@ -18,6 +24,12 @@ function App() {
         </Route>
         <Route path="/register" exact>
           <Register />
+        </Route>
+        <Route path="/app/" exact>
+          <Redirect to="/app/overview" />
+        </Route>
+        <Route path="/app/overview" exact>
+          <Overview />
         </Route>
       </Switch>
     </Router>
--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,43 +1,20 @@
-import { useEffect, useState } from "react";
-import Loading from "../Loading/Loading";
-import { authorize, IUserData } from "../Utils/auth";
+import { Button } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 function HomePage() {
-  let [isLoading, setIsLoading] = useState(true);
-  let [userData, setUserData] = useState<IUserData>({
-    id: "",
-    login: "",
-    email: "",
-    authorized: false,
-  });
-
-  const auth = async (): Promise<boolean> => {
-    let authData: IUserData = await authorize();
-    setUserData(authData);
-    return authData.authorized;
-  };
-
-  useEffect(() => {
-    auth().then((authorized) => {
-      if (authorized) {
-        setIsLoading(false);
-      }
-    });
-  }, []);
-
-  if (isLoading) {
-    return <Loading />;
-  } else {
-    return (
-      <div style={{ display: "flex", flexDirection: "column" }}>
-        <span>Monity</span>
-        <span>TODO: Make this page</span>
-        <span>Logged as:</span>
-        <span>Id: {userData.id}</span>
-        <span>Login: {userData.login}</span>
-        <span>E-mail: {userData.email}</span>
+  return (
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <span>Monity</span>
+      <span>TODO: Make the HomePage</span>
+      <div style={{ width: "300px" }}>
+        <Link to="/app">
+          <Button variant="contained" color="primary">
+            Login
+          </Button>
+        </Link>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
 export default HomePage;
