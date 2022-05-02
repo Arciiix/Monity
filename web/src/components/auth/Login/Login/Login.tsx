edf02b383a083a7e40251fdb0ae590c20adf9fd2
Arciiix <artur.nowak100@gmail.com>
2022-05-02 15:35:11 +0200

    Validate form on login

--- web/src/components/auth/Login/Login/Login.tsx
+++ web/src/components/auth/Login/Login/Login.tsx
@@ -1,15 +1,20 @@
+import { useEffect, useState } from "react";
 import {
   Checkbox,
   Typography,
   Link as MUILink,
   FormControlLabel,
+  InputAdornment,
+  IconButton,
 } from "@mui/material";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import { Link } from "react-router-dom";
+import isEmpty from "validator/lib/isEmpty";
 
 import styles from "./Login.module.css";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 
 interface ILoginProps {
   hideRegister?: boolean;
@@ -18,6 +23,74 @@ interface ILoginProps {
 }
 
 function Login(props: ILoginProps) {
+  let [login, setLogin] = useState("");
+  let [password, setPassword] = useState("");
+  let [rememberMe, setRememberMe] = useState(false);
+
+  let [showPassword, setShowPassword] = useState(false);
+
+  //Contains the error messages of the login form
+  let [errors, setErrors] = useState<{
+    login: string;
+    password: string;
+  }>({
+    login: "",
+    password: "",
+  });
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+  };
+
+  const signIn = async () => {
+    let isThereAnyError = false;
+    let errorObj = { ...errors };
+
+    if (isEmpty(login)) {
+      errorObj.login = "Please enter your login";
+
+      isThereAnyError = true;
+    } else {
+      errorObj.login = "";
+    }
+
+    if (isEmpty(password)) {
+      errorObj.password = "Please enter your password";
+      isThereAnyError = true;
+    } else {
+      errorObj.password = "";
+    }
+
+    setErrors(errorObj);
+
+    if (isThereAnyError) {
+      return;
+    }
+
+    //TODO: Log in
+
+    // If user has successfully logged in and the remember me checkbox is checked,
+    // then save the login in the local storage
+
+    //DEV TODO: Ensure it runs only when user has successfully logged in
+    localStorage.setItem("cachedLogin", login);
+  };
+
+  useEffect(() => {
+    //If there's any cached login in the local storage, set the login to it
+    let cachedLogin = localStorage.getItem("cachedLogin");
+
+    if (cachedLogin) {
+      setLogin(cachedLogin);
+    }
+  }, []);
+
   return (
     <Box
       width={"100%"}
@@ -30,11 +103,46 @@ function Login(props: ILoginProps) {
     >
       <h1>Login</h1>
       <Box width="100%" mb="5px">
-        <TextField label="Login/e-mail" margin="normal" fullWidth />
-        <TextField label="Password" margin="normal" fullWidth />
+        <TextField
+          value={login}
+          onChange={(e) => setLogin(e.target.value)}
+          label="Login/e-mail"
+          error={!!errors.login} //Convert string to boolean
+          helperText={errors.login}
+          margin="normal"
+          fullWidth
+        />
+        <TextField
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          error={!!errors.password} //Convert string to boolean
+          helperText={errors.password}
+          label="Password"
+          type={showPassword ? "text" : "password"}
+          margin="normal"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+          fullWidth
+        />
       </Box>
 
-      <Button fullWidth className={styles.submit} variant="outlined">
+      <Button
+        className={styles.submit}
+        onClick={signIn}
+        fullWidth
+        variant="outlined"
+      >
         Login
       </Button>
 
@@ -42,7 +150,12 @@ function Login(props: ILoginProps) {
         {!props.hideRememberMe && (
           <FormControlLabel
             label="Remember me"
-            control={<Checkbox defaultChecked />}
+            control={
+              <Checkbox
+                value={rememberMe}
+                onChange={(e) => setRememberMe(e.target.checked)}
+              />
+            }
           />
         )}
         {!props.hideResetPassword && (
