bbf7aa4cf13285bc6f3e1d5299d200542adf1250
Arciiix <artur.nowak100@gmail.com>
2022-05-03 22:54:17 +0200

    Start doing the login page

--- back-end/src/auth/auth.controller.ts
+++ back-end/src/auth/auth.controller.ts
@@ -1,13 +1,16 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, HttpCode, Post } from "@nestjs/common";
 import {
   ApiBadRequestResponse,
   ApiConflictResponse,
   ApiCreatedResponse,
+  ApiForbiddenResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
   ApiTags,
 } from "@nestjs/swagger";
 import { AuthService } from "./auth.service";
 
-import { UserRegisterDto, UserReturnDto } from "./dto/user.dto";
+import { UserLoginDto, UserRegisterDto, UserReturnDto } from "./dto/user.dto";
 
 @Controller("auth")
 @ApiTags("auth")
@@ -25,5 +28,17 @@ export class AuthController {
   })
   async register(@Body() user: UserRegisterDto): Promise<UserReturnDto> {
     return await this.authService.register(user);
+  }
+
+  @Post("login")
+  @HttpCode(200)
+  @ApiOkResponse({
+    description: "The user has been successfully logged in",
+    type: UserReturnDto,
+  })
+  @ApiForbiddenResponse({ description: "The password is incorrect" })
+  @ApiNotFoundResponse({ description: "The user does not exist" })
+  async login(@Body() user: UserLoginDto): Promise<UserReturnDto> {
+    return await this.authService.login(user);
   }
 }
--- back-end/src/auth/auth.service.ts
+++ back-end/src/auth/auth.service.ts
@@ -1,7 +1,13 @@
-import { ConflictException, Injectable, Logger } from "@nestjs/common";
+import {
+  ConflictException,
+  ForbiddenException,
+  Injectable,
+  Logger,
+  NotFoundException,
+} from "@nestjs/common";
 import * as argon2 from "argon2";
 import { PrismaService } from "src/prisma/prisma.service";
-import { UserRegisterDto, UserReturnDto } from "./dto/user.dto";
+import { UserLoginDto, UserRegisterDto, UserReturnDto } from "./dto/user.dto";
 
 @Injectable()
 export class AuthService {
@@ -37,6 +43,45 @@ export class AuthService {
       id: createdUser.id,
       email: createdUser.email,
       login: createdUser.login,
+    };
+  }
+
+  async login(user: UserLoginDto): Promise<UserReturnDto> {
+    const foundUser = await this.prismaService.user.findFirst({
+      where: {
+        OR: [{ login: user.login }, { email: user.login }],
+      },
+    });
+
+    if (!foundUser) {
+      this.logger.log(
+        `Tried to login user ${user.login} but they don't exist`,
+        "Auth"
+      );
+      throw new NotFoundException(`User doesn't exist`);
+    }
+
+    const isPasswordValid = await argon2.verify(
+      foundUser.password,
+      user.password
+    );
+
+    if (!isPasswordValid) {
+      this.logger.log(
+        `Tried to login user ${user.login} but the password is incorrect`,
+        "Auth"
+      );
+      throw new ForbiddenException(`Password is incorrect`);
+    }
+
+    //TODO: Generate a token
+
+    this.logger.log(`User ${user.login} has been logged in`, "Auth");
+
+    return {
+      id: foundUser.id,
+      email: foundUser.email,
+      login: foundUser.login,
     };
   }
 }
